821ddea3d2eb1d196642a8f115b56d34aa588e5c
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:32:12 -0500

    Update gear_sets.py

--- src/gear/gear_sets.py
+++ src/gear/gear_sets.py
@@ -1,1 +1,8 @@
+class GearSet:
+    def __init__(self, gear_items):
+        self.gear_items = gear_items
 
+    def apply_to_stats(self, stats):
+        """Apply the full gear set to the stats."""
+        for item in self.gear_items:
+            item.apply_stats(stats)
