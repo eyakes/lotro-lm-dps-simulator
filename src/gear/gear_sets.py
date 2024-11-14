7454a8955e8db63b4631c4dcf2e0ade077a0b5ff
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:57:32 -0500

    Update gear_sets.py

--- src/gear/gear_sets.py
+++ src/gear/gear_sets.py
@@ -1,8 +1,9 @@
 class GearSet:
     def __init__(self, gear_items):
+        """gear_items is expected to be a list of GearItem objects"""
         self.gear_items = gear_items
 
     def apply_to_stats(self, stats):
-        """Apply the full gear set to the stats."""
+        """Apply the full gear set to the stats by applying each GearItem's stats."""
         for item in self.gear_items:
-            item.apply_stats(stats)
+            item.apply_stats(stats)  # Use the apply_stats method of each GearItem
