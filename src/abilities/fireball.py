355750ea62310cbf0edbb82616683106927d4b73
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:28:56 -0500

    Update fireball.py

--- src/abilities/fireball.py
+++ src/abilities/fireball.py
@@ -1,1 +1,9 @@
-
+class Fireball:
+    def __init__(self):
+        self.name = "Fireball"
+        self.damage = 100
+        self.cost = 20  # Mana or resource cost
+        self.cooldown = 3  # Seconds
+        
+    def __repr__(self):
+        return self.name
