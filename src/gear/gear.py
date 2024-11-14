4892a707a317dc7eb6a735a659e16c20221ffde7
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:32:01 -0500

    Update gear.py

--- src/gear/gear.py
+++ src/gear/gear.py
@@ -8,12 +8,3 @@ class GearItem:
         """Apply the gear item stat boosts to the current stats."""
         for stat, value in self.stat_boosts.items():
             stats[stat] += value
-
-class GearSet:
-    def __init__(self, gear_items):
-        self.gear_items = gear_items
-
-    def apply_to_stats(self, stats):
-        """Apply the full gear set to the stats."""
-        for item in self.gear_items:
-            item.apply_stats(stats)
