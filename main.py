dafa4ab3d9af9d2c1584e3f5b18a9c08be168de6
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:52:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 # src/main.py
 from abilities.fireball import Fireball
 from gear.gear import GearItem
