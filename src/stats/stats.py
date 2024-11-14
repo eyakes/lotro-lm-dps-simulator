e66971a962a142d1b49a6d5117e77cc7438416f5
Erik Yakes <erik.t.yakes@gmail.com>
2024-11-14 13:33:23 -0500

    Update stats.py

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -1,10 +1,5 @@
 class Stats:
-    def __init__(self, crit=0, might=0, intellect=0, agility=0):
+    def __init__(self, crit=0, might=0):
         self.crit = crit
         self.might = might
-        self.intellect = intellect
-        self.agility = agility
         # Additional stats can be added here (e.g., for virtues, racial traits, etc.)
-
-    def __repr__(self):
-        return f"Crit: {self.crit}, Might: {self.might}, Intellect: {self.intellect}, Agility: {self.agility}"
